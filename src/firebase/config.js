461c2c04ae871ebda7c60f11be1a87dd72b435cc
ChackWolf <jacksoncheriyan05@gmail.com>
2024-05-18 13:28:12 +0530

    olx first push

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBRZ8L2dzYyfaBkLm-1HFEMYUHLQdPt_mo",
+    authDomain: "olx-clone-18561.firebaseapp.com",
+    projectId: "olx-clone-18561",
+    storageBucket: "olx-clone-18561.appspot.com",
+    messagingSenderId: "215193197989",
+    appId: "1:215193197989:web:a5c599a801adf3f8279c4c",
+    measurementId: "G-S6RJ3KEYZW"
+  };
+
+
+ const firebase = firebase.initializeApp(firebaseConfig);
+
